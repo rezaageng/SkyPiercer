3621b551e42e47a7f9ba5a41cf92438411bfec46
rezaa <rezaageng5@gmail.com>
2025-06-09 20:00:04 +0700

    feat: add spread bullet

--- Assets/Scripts/EnemyGun.cs
+++ Assets/Scripts/EnemyGun.cs
@@ -6,10 +6,22 @@ public class EnemyGun : MonoBehaviour
 {
     public GameObject EnemyBullet;
     public float fireRate = 2f;
+    public int numberOfBullets = 5; // Number of bullets in the spread
+    public float spreadAngle = 45f; // The angle of the spread
 
     void Start()
     {
-        Invoke("FireEnemyBullet", fireRate);
+
+    }
+
+    void OnEnable()
+    {
+        InvokeRepeating("FireEnemyBullet", 1f, fireRate);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke("FireEnemyBullet");
     }
 
     void FireEnemyBullet()
@@ -17,13 +29,19 @@ public class EnemyGun : MonoBehaviour
         GameObject playerShip = GameObject.Find("PlayerGo");
         if (playerShip != null)
         {
-            GameObject bullet = Instantiate(EnemyBullet);
-            bullet.transform.position = transform.position;
+            Vector2 directionToPlayer = (playerShip.transform.position - transform.position).normalized;
+            float angleStep = spreadAngle / (numberOfBullets - 1);
+            float startAngle = -spreadAngle / 2;
 
-            Vector2 direction = playerShip.transform.position - transform.position;
-            bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+            for (int i = 0; i < numberOfBullets; i++)
+            {
+                float angle = startAngle + (i * angleStep);
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                Vector2 direction = rotation * directionToPlayer;
+
+                GameObject bullet = Instantiate(EnemyBullet, transform.position, Quaternion.identity);
+                bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+            }
         }
-
-        Invoke("FireEnemyBullet", fireRate);
     }
 }
