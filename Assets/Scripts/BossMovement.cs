ae25119a4ac43a8ca5648b89b085cda9b89f3df6
rezaa <rezaageng5@gmail.com>
2025-06-12 09:56:43 +0700

    refactor: clean up and improve readebility

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -16,12 +16,11 @@ public class BossMovement : MonoBehaviour
         Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-        // Atur area gerak boss agar tidak terlalu lebar & tidak terlalu sempit
-        float horizontalArea = (max.x - min.x) * 0.6f; // 60% area horizontal (bisa diubah 0.5f - 0.7f sesuai selera)
+        float horizontalArea = (max.x - min.x) * 0.6f;
         float centerX = (min.x + max.x) / 2f;
 
-        float verticalTop = max.y - 1f;     // batas atas boss
-        float verticalBottom = max.y - 3f;  // batas bawah boss (tidak terlalu turun ke player)
+        float verticalTop = max.y - 1f;
+        float verticalBottom = max.y - 3f;
 
         minBounds = new Vector2(centerX - horizontalArea / 2f, verticalBottom);
         maxBounds = new Vector2(centerX + horizontalArea / 2f, verticalTop);
